51306f7e264d39faba3904ca4c562bf53d14b2da
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2020-06-12 17:27:13 -0400

    Scrolling pages

---
+++ resources/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready( function() {
+
+    var active = window.location.href.split('#')[1];
+    $('.navigator[data-href="'+active+'"').addClass('active');
+    scrollTo(active);
+
+    
+    $('.navigator').on('click', function() {
+        var page = $(this).data('href');
+        window.location.href = '#'+page;
+
+        var fromPage  = $('.navigator.active');
+        fromPage.removeClass('active');
+        
+        $('.navigator[data-href="'+page+'"').addClass('active');
+        scrollTo(page);
+    });
+
+    
+    function scrollTo(page) {
+        $('.main-content').animate({
+            scrollTop: $('.main-content').scrollTop() - $('.main-content').offset().top + $('.page[data-page="'+page+'"]').position().top
+        }, 500);
+    }
+
+});
