700b53819fec69c5afd84e3f74599fdb91f89fe3
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2023-10-24 23:58:36 -0400

    Fixed javascript

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,35 +2,37 @@ $(document).ready( function() {
 
     $.getJSON('/resources/data/site-content.json', function(jd) {
         $("#page-body").tmpl(jd).appendTo("#body");
+
+        var active = window.location.href.split('#')[1];
+        $('.navigator[data-href="'+active+'"').addClass('active');
+        if (active)
+            scrollTo(active);
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $('.navigator').on('click', function() {
+            var page = $(this).data('href');
+            window.location.href = '#'+page;
+
+            var fromPage  = $('.navigator.active');
+            fromPage.removeClass('active');
+            
+            $('.navigator[data-href="'+page+'"').addClass('active');
+            scrollTo(page);
+        });
+
+        $('.download-cv').on('click', function() {
+            window.open('./resources/assets/downloads/Prithviraj%20Chaudhuri%20Resume.pdf' , '_blank');
+        });
+
+        
+        function scrollTo(page) {
+            $('.main-content').animate({
+                scrollTop: $('.main-content').scrollTop() - $('.main-content').offset().top + $('.page[data-page="'+page+'"]').position().top
+            }, 500);
+        }
      });
 
-    var active = window.location.href.split('#')[1];
-    $('.navigator[data-href="'+active+'"').addClass('active');
-    if (active)
-        scrollTo(active);
-
-    $('[data-toggle="tooltip"]').tooltip();
-
-    $('.navigator').on('click', function() {
-        var page = $(this).data('href');
-        window.location.href = '#'+page;
-
-        var fromPage  = $('.navigator.active');
-        fromPage.removeClass('active');
-        
-        $('.navigator[data-href="'+page+'"').addClass('active');
-        scrollTo(page);
-    });
-
-    $('.download-cv').on('click', function() {
-        window.open('./resources/assets/downloads/Prithviraj%20Chaudhuri%20Resume.pdf' , '_blank');
-    });
-
     
-    function scrollTo(page) {
-        $('.main-content').animate({
-            scrollTop: $('.main-content').scrollTop() - $('.main-content').offset().top + $('.page[data-page="'+page+'"]').position().top
-        }, 500);
-    }
 
 });
