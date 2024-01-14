b0839914bfe5f4e932ec983702a30e7d1b5845de
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-01-13 19:06:50 -0500

    V3.11 (#23)
    * added local dev server
    
    * updated content and format

---
+++ server.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 8080;
+var express = require("express");
+const data = require('./resources/data/site-content.json')
+
+var app = express();
+app.use(express.static(__dirname)); //use static files in ROOT/public folder
+
+app.get("/data", function(request, response){ //root dir
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(data));
+});
+
+app.listen(port, host);
