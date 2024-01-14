41cc0e40eaddd00d3cd9b66cf7c6f38581ce2563
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-01-13 19:05:52 -0500

    updated content and format

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var fs = require("fs");
 var host = "127.0.0.1";
 var port = 8080;
 var express = require("express");
+const data = require('./resources/data/site-content.json')
 
 var app = express();
 app.use(express.static(__dirname)); //use static files in ROOT/public folder
 
-app.get("/", function(request, response){ //root dir
-    response.send("Hello!!");
+app.get("/data", function(request, response){ //root dir
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(data));
 });
 
 app.listen(port, host);
